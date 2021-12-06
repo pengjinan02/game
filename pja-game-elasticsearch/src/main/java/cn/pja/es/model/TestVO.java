da7f801cb00943332000440dece9167217389ac2
pengjinan02 <1354781522@qq.com>
2021-12-07 00:16:56 +0800

    es

---
+++ pja-game-elasticsearch/src/main/java/cn/pja/es/model/TestVO.java
@@ -0,0 +1,23 @@
+package cn.pja.es.model;
+
+import lombok.Data;
+
+/**
+ * @author 彭金安
+ * @DATE 2021/12/6
+ */
+@Data
+public class TestVO {
+    /**
+     * id
+     */
+    private Integer id;
+    /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 描述
+     */
+    private String describe;
+}
